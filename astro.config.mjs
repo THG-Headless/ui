c57fa317dc00e303aa1eb25541a15fc459616f50
Joe Johnson <01johnson.joe@gmail.com>
2024-11-25 09:54:07 +0000

    Applied altitude styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [starlight({
     title: 'Altitude Standalone Components',
+    customCss: ["/src/styles.css", "@fontsource-variable/inter"],
+     logo: {
+        replacesTitle: true,
+        light: "/src/assets/logo-long-light.svg",
+        dark: "/src/assets/logo-long.svg",
+        link: "https://thgaltitude.com/",
+      },
     sidebar: [
       {
         label: 'Components',
