a07af22d1bbad0ccbfb997ec8b4326e91a71d862
Joe Johnson <01johnson.joe@gmail.com>
2024-11-27 11:22:29 +0000

    Added storybook for accessibility testing

---
+++ .storybook/main.js
@@ -0,0 +1,6 @@
+// .storybook/main.js
+module.exports = {
+  stories: ['../src/stories/*.js'],
+  addons: ['@storybook/addon-a11y'],
+  framework: '@storybook/html-webpack5',
+};
---
+++ src/stories/Buttons.stories.js
@@ -0,0 +1,28 @@
+import AccentButton from '../components/buttons/AccentButton.html';
+import PrimaryButton from '../components/buttons/PrimaryButton.html';
+import SecondaryButton from '../components/buttons/SecondaryButton.html';
+import StatusButtons from '../components/buttons/StatusButtons.html';
+
+import "@thg/standalone-components-css";
+
+export default {
+  title: 'Buttons',
+  parameters: {
+    a11y: {
+      config: {
+      },
+    },
+  },
+};
+
+export const Primary = () => PrimaryButton;
+Primary.storyName = 'Primary Button';
+
+export const Secondary = () => SecondaryButton;
+Secondary.storyName = 'Secondary Button';
+
+export const Accent = () => AccentButton;
+Accent.storyName = 'Accent Button';
+
+export const Status = () => StatusButtons;
+Status.storyName = 'Status Buttons';
