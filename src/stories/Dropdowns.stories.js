dc56e8af61329dd0458ff0371376378ad5d72c8c
Joe Johnson <01johnson.joe@gmail.com>
2024-12-05 19:01:25 +0000

    Initial implementaion of dropdown

---
+++ src/stories/Dropdowns.stories.js
@@ -0,0 +1,14 @@
+import BasicDropdown from '../components/dropdown/BasicDropdown.html';
+
+export default {
+  title: 'Dropdowns',
+  parameters: {
+    a11y: {
+      config: {
+      },
+    },
+  },
+};
+
+export const Basic = () => BasicDropdown;
+Basic.storyName = 'Basic Dropdown';
