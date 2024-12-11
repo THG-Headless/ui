7be7a5b49fe6ec3230e0bd93971ed8ff7d06da63
Joe Johnson <01johnson.joe@gmail.com>
2024-12-11 15:44:09 +0000

    Added variants, accessibility and documentation

--- src/stories/Dropdowns.stories.js
+++ src/stories/Dropdowns.stories.js
@@ -1,4 +1,7 @@
 import BasicDropdown from '../components/dropdown/BasicDropdown.html';
+import ErrorDropdown from '../components/dropdown/ErrorDropdown.html';
+import InactiveDropdown from '../components/dropdown/InactiveDropdown.html';
+import SearchDropdown from '../components/dropdown/SearchDropdown.html'
 
 export default {
   title: 'Dropdowns',
@@ -12,3 +15,12 @@ export default {
 
 export const Basic = () => BasicDropdown;
 Basic.storyName = 'Basic Dropdown';
+
+export const Error = () => ErrorDropdown;
+Error.storyName = 'Error Dropdown';
+
+export const Inactive = () => InactiveDropdown;
+Inactive.storyName = 'Inactive Dropdown';
+
+export const Search = () => SearchDropdown;
+Search.storyName = 'Search Dropdown';
