efc3e87dc6cfd09a945ca0b6c21fe22196e52dd5
Joe Johnson <01johnson.joe@gmail.com>
2025-01-17 12:04:02 +0000

    Accessibility and UX improvements

--- src/stories/Carousels.stories.js
+++ src/stories/Carousels.stories.js
@@ -1,17 +1,17 @@
-import BasicCarousel from "../components/carousel/BasicCarousel.html";
-import ProductRail_withBulletsAndControls from "../components/carousel/ProductRail_withBulletsAndControls.html";
+// import BasicCarousel from "../components/carousel/BasicCarousel.html";
+// import ProductRail_withBulletsAndControls from "../components/carousel/ProductRail_withBulletsAndControls.html";
 
-export default {
-  title: "Carousels",
-  parameters: {
-    a11y: {
-      config: {},
-    },
-  },
-};
+// export default {
+//   title: "Carousels",
+//   parameters: {
+//     a11y: {
+//       config: {},
+//     },
+//   },
+// };
 
-export const Hero = () => BasicCarousel;
-Hero.storyName = "Hero Carousel";
+// export const Hero = () => BasicCarousel;
+// Hero.storyName = "Hero Carousel";
 
-export const ProductList = () => ProductRail_withBulletsAndControls;
-ProductList.storyName = "Product List Carousel";
+// export const ProductList = () => ProductRail_withBulletsAndControls;
+// ProductList.storyName = "Product List Carousel";
